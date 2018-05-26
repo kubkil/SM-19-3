7ec03fc5d9156228169fac6a38e64ac345eb16c5
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-26 22:38:28 +0200

    refactor

--- actions.js
+++ actions.js
@@ -1,77 +1,37 @@
 import uuid from uuid;
+
+// zmienne potrzebne? export potrzebne?
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 export const THUMB_UP = 'THUMB_UP';
 export const THUMB_DOWN = 'THUMB_DOWN';
 
-{
+export const addComment = text => ({
   type: ADD_COMMENT,
-  id,
-  text,
-  author
-}
+  id: uuid.v4(),
+  text
+})
 
-{
+export const editComment = (text, id) => ({
   type: EDIT_COMMENT,
   id,
   text
-}
+})
 
-{
+export const removeComment = id => ({
   type: REMOVE_COMMENT,
   id
-}
+})
 
-{
+// to samo id co w comment?
+// index zamiast id?
+export const thumbUp = commentId => ({
   type: THUMB_UP,
-  likesNumber
-}
+  id: commentId
+})
 
-{
+export const thumbDown = commentId => ({
   type: THUMB_DOWN,
-  dislikesNumber
-}
-
-function addComment(text) {
-  return {
-    type: ADD_COMMENT,
-    text,
-    id: uuid.v4()
-  }
-}
-
-function editComment(text, id) {
-  return {
-    type: EDIT_COMMENT,
-    text,
-    id
-  }
-}
-
-function removeComment(id) {
-  return {
-    type: REMOVE_COMMENT,
-    id
-  }
-}
-
-function thumbUp(likesNumber) {
-  return {
-    type: THUMB_UP,
-    likesNumber
-  }
-}
-
-function thumbDown(dislikesNumber) {
-  return {
-    type: THUMB_DOWN,
-    dislikesNumber
-  }
-}
-
-const boundAddComment = text => dispatch(addComment(text));
-const boundEditComment = text => dispatch(editComment(text, id));
-const boundRemoveComment = text => dispatch(removeComment(id));
-const boundThumbUp = text => dispatch(thumbUp(likesNumber));
-const boundThumbDown = text => dispatch(thumbDown(dislikesNumber));
+  id: commentId
+})
